6af94609dc9d78796a586c98e7949270c0454776
Katyani27 <154884424+Katyani27@users.noreply.github.com>
2024-08-23 14:41:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstWord(s) {
   // your code here
+	 let spaceIndex = s.indexOf(' ');
+
+    // If there is no space, return the entire string
+    if (spaceIndex === -1) {
+        return s;
+    }
+
+    // Return the substring from the start to the first space
+    return s.substring(0, spaceIndex);
 }
 
 // Do not change the code below
